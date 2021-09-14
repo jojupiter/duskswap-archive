ab84db148255e91dd2b041d350959f6a22cabc39
SCrf1 <srgseraphin14@gmail.com>
2021-09-14 14:38:21 +0200

    fixing sec bug btcpay webhook

--- src/main/java/com/dusk/duskswap/application/securityConfigs/WebSecurityConfig.java
+++ src/main/java/com/dusk/duskswap/application/securityConfigs/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**",
-                "/deposits/update-status",
+                "/deposits/received-payment",
                 "/newsletters/add-subscriber",
                 "/users/check-email",
                 "/users/update-password").permitAll()
